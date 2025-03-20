5295bc259c183fdc661c4307931776da6ba14e15
ufywufy <64656635+ufywufy@users.noreply.github.com>
2025-03-19 22:38:53 -0700

    Update AppFactory.java

--- mvcApps/src/mvc/AppFactory.java
+++ mvcApps/src/mvc/AppFactory.java
@@ -1,11 +1,12 @@
 package mvc;
 
 public interface AppFactory {
-    Model makeModel();
+
     View makeView(Model model);
+    Model makeModel();
     String getTitle();
-    String[] getHelp();
+    String getHelp();
     String about();
     String[] getEditCommands();
-    Command makeEditCommand(Model model, String type, Object source);
+    Command makeEditCommand(Model model, String type);
 }
