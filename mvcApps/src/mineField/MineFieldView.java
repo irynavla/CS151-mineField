026b160540aa5df71e0c33036c3752313f786acd
ufywufy <64656635+ufywufy@users.noreply.github.com>
2025-03-19 22:38:06 -0700

    Update MineFieldView.java
    fixed

--- mvcApps/src/mineField/MineFieldView.java
+++ mvcApps/src/mineField/MineFieldView.java
@@ -1,35 +1,71 @@
 package mineField;
 
-import mvc.View;
+import mvc.*;
+import javax.swing.*;
 import java.awt.*;
 
 public class MineFieldView extends View {
-    private static final int CELL_SIZE = 30;
+    private MineFieldModel model;
+    private JButton[][] gridCells;
+    private int rows, cols;
+    private JPanel gridPanel;
 
     public MineFieldView(MineFieldModel model) {
         super(model);
+        this.model = model;
+        this.rows = model.getRows();
+        this.cols = model.getCols();
+        this.gridCells = new JButton[rows][cols];
+
+        setLayout(new BorderLayout());
+
+        gridPanel = new JPanel();
+        gridPanel.setLayout(new GridLayout(rows, cols));
+        add(gridPanel, BorderLayout.CENTER);
+
+        initView();
+        model.subscribe(this);
+        update();
+    }
+
+    private void initView() {
+        gridPanel.removeAll();
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                JButton cell = new JButton("?");
+                cell.setFont(new Font("Arial", Font.PLAIN, 10));
+                cell.setEnabled(false);
+                cell.setBackground(Color.DARK_GRAY);
+                cell.setForeground(Color.WHITE);
+                gridCells[r][c] = cell;
+                gridPanel.add(cell);
+            }
+        }
     }
 
     @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        MineFieldModel field = (MineFieldModel) model;
-
-        for (int r = 0; r < field.getRows(); r++) {
-            for (int c = 0; c < field.getCols(); c++) {
-                if (field.isMine(r, c)) {
-                    g.setColor(Color.RED);
+    public void update() {
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                JButton cell = gridCells[r][c];
+                if (model.isVisited(r, c)) {
+                    cell.setBackground(Color.LIGHT_GRAY);
+                    int neighborMines = model.getNeighborMines(r, c);
+                    cell.setText(Integer.toString(neighborMines));
                 } else {
-                    g.setColor(Color.LIGHT_GRAY);
+                    cell.setBackground(Color.DARK_GRAY);
+                    cell.setText("?");
                 }
-                g.fillRect(c * CELL_SIZE, r * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-                g.setColor(Color.BLACK);
-                g.drawRect(c * CELL_SIZE, r * CELL_SIZE, CELL_SIZE, CELL_SIZE);
             }
         }
+        repaint();
+    }
 
-        // Draw player
-        g.setColor(Color.BLUE);
-        g.fillOval(field.getPy() * CELL_SIZE, field.getPx() * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    public void setModel(MineFieldModel m) {
+        super.setModel(m);
+        this.rows = m.getRows();
+        this.cols = m.getCols();
+        m.subscribe(this);
+        repaint();
     }
 }
