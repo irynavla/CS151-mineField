c6bdfb1c6340b94af1a008c2a14d16cef6d7216c
ufywufy <64656635+ufywufy@users.noreply.github.com>
2025-03-20 21:35:33 -0700

    Update MineFieldFactory.java
    names

--- mvcApps/src/mineField/MineFieldFactory.java
+++ mvcApps/src/mineField/MineFieldFactory.java
@@ -17,7 +17,7 @@ public class MineFieldFactory implements AppFactory {
     }
 
     public String about() {
-        return "MineField Game\nVersion 1.0\nDeveloped by Iryna Vlasiuk";
+        return "MineField Game\nVersion 1.0\nDeveloped by Iryna Vlasiuk, Emily Williams, ";
     }
 
     public String[] getEditCommands() {
